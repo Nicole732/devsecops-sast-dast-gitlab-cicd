527229cb3ac1a44f29c289b95a5405fd1415e36e
Nicole Ketchiozo <dketchiozo@gmail.com>
2024-07-21 19:10:03 -0400

    automated all types of findings

--- upload-reports.py
+++ upload-reports.py
@@ -1,4 +1,15 @@
 import requests
+import sys
+
+file_name = sys.argv[1]
+scan_type = ''
+
+if file_name == 'gitleasks.json':
+    scan_type = 'Gitleaks Scan'
+elif file_name == 'njscan.serif':
+    scan_type = 'SARIF'
+elif file_name == 'semgrep.json':
+    scan_type = 'Semgrep JSON Report'
 
 headers = {
     'Authorization': 'Token 548afd6fab3bea9794a41b31da0e9404f733e222'
@@ -10,13 +21,13 @@ data = {
     
     'active': True,
     'verified': True,
-    'scan_type': 'Gitleaks Scan',
+    'scan_type': scan_type,
     'minimum_severity': 'Low',
     'engagement': 19
 }
 
 files = {
-    'file': open('gitleaks.json', 'rb')
+    'file': open(file_name, 'rb')
 }
 
 response = requests.post(url, headers=headers, data=data, files=files)
