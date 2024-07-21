a193700b851959f178005c35b328a6ca712132ce
Nicole Ketchiozo <dketchiozo@gmail.com>
2024-07-21 17:15:05 -0400

    automated defectdojo findings in the pipeline using python

---
+++ upload-reports.py
@@ -0,0 +1,27 @@
+import requests
+
+headers = {
+    'Authorization': 'Token 548afd6fab3bea9794a41b31da0e9404f733e222'
+}
+
+url = 'https://demo.defectdojo.org/api/v2/import-scan/'
+
+data = {
+    
+    'active': True,
+    'verified': True,
+    'scan_type': 'Gitleaks Scan',
+    'minimum_severity': 'Low',
+    'engagement': 35
+}
+
+files = {
+    'file': open('gitleaks.json', 'rb')
+}
+
+response = requests.post(url, headers=headers, data=data, files=files)
+
+if response.status_code == 201:
+    print('Scan imported successfully')
+else:
+    print('Failed to import scan results: {response.content}')
